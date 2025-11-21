72c0fedd7a90bdbd0e667309f50832f197b63595
Esteban Garcia <58041676+KossTV@users.noreply.github.com>
2025-11-21 08:40:53 +0100

    proyecto spring boot inicializado sin nada

---
+++ Backend/epickeys/src/main/java/com/daw/epickeys/EpickeysApplication.java
@@ -0,0 +1,13 @@
+package com.daw.epickeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpickeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpickeysApplication.class, args);
+	}
+
+}
---
+++ Backend/epickeys/src/test/java/com/daw/epickeys/EpickeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.epickeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpickeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
