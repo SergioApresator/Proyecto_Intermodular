0da664ac8512b82c3f0dab6ed23db1977695c233
EstebanPuntoDev <kossstv@gmail.com>
2026-05-10 15:17:03 +0200

    Merge branch 'develop' into bugs/bug-cerrar-sesion

---
+++ Frontend/src/app/acerca-de/acerca-de.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Footer } from '../footer/footer';
+
+@Component({
+  selector: 'app-acerca-de',
+  standalone: true,
+  imports: [Footer],
+  templateUrl: './acerca-de.html',
+  styleUrl: './acerca-de.css',
+})
+export class AcercaDe {
+
+}
